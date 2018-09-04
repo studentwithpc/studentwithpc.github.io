1667b356d3e19a263adeb051b58bc110b2fdb07e
Valeria <kossiopia91@gmail.com>
2018-09-04 13:16:13 +0300

    translater v1.2 formalis js

--- scripts/main.js
+++ scripts/main.js
@@ -1,46 +1,46 @@
+'use strict'
 // не работает в IE
-vocabulary = {"кот":"cat",
-"объект":"object",
-"форма":"form",
-"код":"code",
+const vocabulary = {
+  'кот': 'cat',
+  'объект': 'object',
+  'форма': 'form',
+  'код': 'code'
 // ... end demo
-};
-mykeys = Object.keys(vocabulary); 
-var ruButton = document.getElementById('ru'),
-engButton = document.getElementById('eng'),
-engField = document.getElementById('engtext'),
-ruField = document.getElementById('rutext');
-
-RuToEng_f = function(text)
-{ var ans = 'word not found';
-for(key in vocabulary)
-{
-	if(text === key)
-	{ans = vocabulary[key];}
 }
-	return ans;}
+const mykeys = Object.keys(vocabulary)
+let ruButton = document.getElementById('ru')
+let engButton = document.getElementById('eng')
+let engField = document.getElementById('engtext')
+let ruField = document.getElementById('rutext')
 
-EngToRu_f = function(text)
-{ var ans = 'слово не найдено';
-	for(var i =0; i<mykeys.length; i++)
-	{
-		if(vocabulary[mykeys[i]] === text) 
-		{ans = mykeys[i]}
-	}
-	return ans;
+function RuToEngF (text) {
+  let ans = 'word not found'
+  for (let key in vocabulary) {
+    if (text === key) {
+      ans = vocabulary[key]
+    }
+  }
+  return ans
 }
 
-engButton.onclick = function() {
-	engField.value=RuToEng_f(ruField.value);
- engField.style.border  = '2px solid blue';
-	ruField.style.border  = '1px solid black';
-	
+function EngToRuF (text) {
+  let ans = 'слово не найдено'
+  for (let i = 0; i < mykeys.length; i++) {
+    if (vocabulary[mykeys[i]] === text) {
+      ans = mykeys[i]
+    }
+  }
+  return ans
 }
 
-ruButton.onclick = function() {
+engButton.onclick = function () {
+  engField.value = RuToEngF(ruField.value)
+  engField.style.border = '2px solid blue'
+  ruField.style.border = '1px solid black'
+}
 
-	ruField.value =EngToRu_f(engField.value);
-	ruField.style.border  = '2px solid blue';
-	engField.style.border  = '1px solid black';
-
+ruButton.onclick = function () {
+  ruField.value = EngToRuF(engField.value)
+  ruField.style.border = '2px solid blue'
+  engField.style.border = '1px solid black'
 }
